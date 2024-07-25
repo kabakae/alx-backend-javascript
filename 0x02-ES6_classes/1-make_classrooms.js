585f271e52b91a30f77b40989fa21572f0ee278b
kabakae <kabakaevans@gmail.com>
2024-07-25 16:50:21 +0000

    00

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,5 @@
-import ClassRoom from './0-classroom.js';
-function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
-  ];
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
-export default initializeRooms;
