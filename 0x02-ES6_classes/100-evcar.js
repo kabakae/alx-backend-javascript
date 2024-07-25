4029445add5cbf1c497ba62f5fcb66ad25409cff
kabakae <kabakaevans@gmail.com>
2024-07-25 19:15:41 +0000

    00

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,19 +2,14 @@ import Car from './10-car.js';
 
 class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(); // Call the parent class constructor if needed
+    super(); 
     this._brand = brand;
     this._motor = motor;
     this._color = color;
     this._range = range;
   }
-
-  // Override the cloneCar method
   cloneCar() {
-    // Return a new instance of Car
     return new Car();
   }
 }
-
 export default EVCar;
-
