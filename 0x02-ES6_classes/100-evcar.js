c7061ae2c36d71c9b6ffbb1654999c97cf0d4f09
kabakae <kabakaevans@gmail.com>
2024-07-25 17:41:19 +0000

    00

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,26 +1,12 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, motor, color); // Initialize parent class
-    this._range = range; // Initialize the range specific to EVCar
-
-    // Define properties with underscore for private attributes
-    Object.defineProperties(this, {
-      _brand: { value: brand, writable: true },
-      _motor: { value: motor, writable: true },
-      _color: { value: color, writable: true },
-      _range: { value: range, writable: true }
-    });
+    super(brand, motor, color);
+    this._range = range;
   }
 
-  // Override cloneCar method
-  cloneCar() {
-    const car = new Car();
-    car._brand = this._brand;
-    car._motor = this._motor;
-    car._color = this._color;
-    return car;
+  static get [Symbol.species]() {
+    return Car;
   }
 }
-
