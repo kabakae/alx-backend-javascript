0f94c0de47f4777d64f9e52e9818bccf361dba82
kabakae <kabakaevans@gmail.com>
2024-07-25 18:22:56 +0000

    00

--- 0x02-ES6_classes/.eslintrc.js
+++ 0x02-ES6_classes/.eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-        jest: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'airbnb',
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
     ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
-    plugins: [
-        'react',
-    ],
-    rules: {
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-        'no-console': 'warn',
-        'no-underscore-dangle': 'off',
-        'no-multiple-empty-lines': ['error', { max: 0 }],
-        'quotes': ['error', 'single'],
-        'import/extensions': ['error', 'never'],
-        'semi': ['error', 'always'],
-    },
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
-
