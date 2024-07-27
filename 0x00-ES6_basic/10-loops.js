a0f64961d197e39a7e479f62588fb4b2cf07a6fe
kabakae <kabakaevans@gmail.com>
2024-07-27 11:06:55 +0000

    00

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx of array) {
-    array[array.indexOf(idx)] = appendString + idx;
-  }
-
-  return array;
+  return array.map((value) => appendString + value);
 }
