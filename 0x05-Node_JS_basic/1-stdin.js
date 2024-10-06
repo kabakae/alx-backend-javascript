74fbf661c92df81b1d33ac067f2f416749ee8c76
kabakae <kabakaevans064@gmail.com>
2024-10-06 17:21:50 +0000

    00

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,14 +6,17 @@ console.log('Welcome to Holberton School, what is your name?');
 // Read input from stdin
 process.stdin.setEncoding('utf-8');
 
-// Handle data input from user
 process.stdin.on('data', (input) => {
   const name = input.trim(); // Trim any extra whitespace/newlines from the input
   console.log(`Your name is: ${name}`);
-  
-  // Display closing message
-  console.log('This important software is now closing');
 
-  // Exit the process
-  process.exit();
+  if (process.stdin.isTTY) {
+    // TTY mode (interactive terminal), manually close when user presses Enter
+    process.exit();
+  }
+});
+
+// When stdin is closed (in case of piping input), display the closing message
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
