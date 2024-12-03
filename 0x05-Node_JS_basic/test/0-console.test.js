e00f1b3a12c38fd31e9c7d2f415abc510e41113b
kabakae <kabakaevans064@gmail.com>
2024-12-03 04:34:04 +0000

    00

--- 0x05-Node_JS_basic/test/0-console.test.js
+++ 0x05-Node_JS_basic/test/0-console.test.js
@@ -1,15 +1,20 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
-const displayMessage = require('../0-console');
+const { expect } = require('chai'); // Import Chai for assertions
+const sinon = require('sinon');     // Import Sinon for spying on console.log
+const displayMessage = require('../0-console'); // Import the function to test
 
 describe('displayMessage', () => {
     it('should log the correct message to the console', () => {
+        // Create a spy on console.log
         const consoleSpy = sinon.spy(console, 'log');
-        const message = "Hello NodeJS!";
+        const message = "Hello NodeJS!"; // Expected message
 
+        // Call the function
         displayMessage(message);
 
+        // Assert that console.log was called once with the correct message
         expect(consoleSpy.calledOnceWithExactly(message)).to.be.true;
+
+        // Restore the original console.log behavior
         consoleSpy.restore();
     });
 });
